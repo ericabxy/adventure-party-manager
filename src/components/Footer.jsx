d39336beca256f7f563c72f8de4fc7fcbd7d9a32
Eric Abides <ericxdu23@gmail.com>
2025-09-02 16:09:26 -0700

    reduce spacing between notices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 function Footer ({  }) {
   return (
     <footer>
-      <p style={{textAlign: 'center'}}>
+      <div style={{textAlign: 'center'}}>
         <small>
           Application programming &copy; 2025 by Eric Abides.
         </small>
-      </p>
-      <p style={{textAlign: 'center'}}>
+      </div>
+      <div style={{textAlign: 'center'}}>
         <small>
           Character art by Charles Gabriel.
           Commissioned by <a target="_blank" href="http://opengameart.org">
             OpenGameArt.org
           </a>
         </small>
-      </p>
+      </div>
     </footer>
   )
 }
